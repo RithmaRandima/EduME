9b82138341ca83d9f03c16acf03ca8043a17339d
RithmaRandima <rithma.r.nandasiri.com>
2025-06-01 21:18:52 +0530

    Completed About Page and Section

--- src/components/about/AWrapper.jsx
+++ src/components/about/AWrapper.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { homeAbout } from "../../assests/dummyData";
 
 const AWrapper = () => {
   return (
     <>
-      <section className=""></section>
+      <section className="awrapper">
+        <div className="container grid">
+          {homeAbout.map((val) => (
+            <div className="box flex">
+              <div className="img">
+                <img src={val.img} alt="" />
+              </div>
+              <div className="text">
+                <h1>{val.data}</h1>
+                <h3>{val.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
